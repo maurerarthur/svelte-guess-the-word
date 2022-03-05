222253a7576a4646a21a1b62daafc640c77fb36c
Arthur Maurer <30410972+maurerarthur@users.noreply.github.com>
2022-03-05 18:19:38 -0300

    Visual adjustments concluded

--- src/App.svelte
+++ src/App.svelte
@@ -2,7 +2,7 @@
   import Wordle from './Wordle.svelte';
 </script>
 
-<div class="h-100 container">
+<div class="h-100 container-fluid">
   <div class="h-100 row justify-content-center">
     <Wordle />
   </div>
--- src/Wordle.svelte
+++ src/Wordle.svelte
@@ -20,28 +20,61 @@
     }
 </script>
 
-<div class="h-100 d-flex flex-column align-items-center justify-content-center">
-    <div>
-        {#each userInput as _, input}
-            <input
-                type='text'
-                class={userInput[input] == word[input] ? 'green' : ''}
-                bind:value={userInput[input]}
-            />
-        {/each}
+<div class="background container-fluid">
+    <div class="h-100 d-flex flex-column align-items-center justify-content-center">
+        <h3 class="mb-4">Wordle</h3>
+        <div>
+            {#each userInput as _, input}
+                <input
+                    type="text"
+                    class={userInput[input] == word[input] ? "correct_letter" : ""}
+                    bind:value={userInput[input]}
+                />
+            {/each}
+        </div>
+        {#if isWordDiscovered}
+            <p class="mt-4">That's right. The word is {word}</p>
+        {/if}
     </div>
-    {#if isWordDiscovered}
-        <p>That's right. The word is {word}</p>
-    {/if}
 </div>
 
 <style>
-    input {
-        height: 30px;
-        width: 30px;
+    :root {
+        --purple: #503047;
+        --green: #ADC698;
+        --light-white: #D0E3C4;
     }
 
-    .green {
-        color: green 1px solid;
+    @import url("https://fonts.googleapis.com/css2?family=League+Gothic&display=swap");
+
+    input {
+        height: 60px;
+        width: 60px;
+        margin: 5px;
+        color: var(--purple);
+        border: none;
+        outline: none;
+        text-align: center;
+        font-size: 40px;
+        font-family: "League Gothic", sans-serif;
+    }
+
+    h3 {
+        font-family: "League Gothic", sans-serif;
+        color: var(--light-white);
+        text-transform: uppercase;
+    }
+
+    p {
+        font-family: "League Gothic", sans-serif;
+        color: var(--light-white);
+    }
+
+    .background {
+        background-color: var(--purple);
+    }
+
+    .correct_letter {
+        background-color: var(--green);
     }
 </style>
